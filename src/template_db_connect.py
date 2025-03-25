00d62cb858f75e861bcdc9eeb9111703b9968e02
Mathias Stadler <stadler-mathias@web.de>
2025-03-25 15:19:30 +0100

    save

---
+++ src/template_db_connect.py
@@ -0,0 +1,62 @@
+import logging
+import time
+import mysql.connector
+
+# Set up logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# Log to console
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Also log to a file
+file_handler = logging.FileHandler("cpy-errors.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler) 
+
+def connect_to_mysql(config, attempts=3, delay=2):
+    attempt = 1
+    # Implement a reconnection routine
+    while attempt < attempts + 1:
+        try:
+            return mysql.connector.connect(**config)
+        except (mysql.connector.Error, IOError) as err:
+            if (attempts is attempt):
+                # Attempts to reconnect failed; returning None
+                logger.info("Failed to connect, exiting without a connection: %s", err)
+                return None
+            logger.info(
+                "Connection failed: %s. Retrying (%d/%d)...",
+                err,
+                attempt,
+                attempts-1,
+            )
+            # progressive reconnect delay
+            time.sleep(delay ** attempt)
+            attempt += 1
+    return None
+
+
+# from myconnection import connect_to_mysql
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': '127.0.0.1',
+  'database': 'mysql',
+  'raise_on_warnings': True
+}
+cnx = connect_to_mysql(config, attempts=3)
+
+if cnx and cnx.is_connected():
+
+    with cnx.cursor() as cursor:
+
+        cnx.close()
+
+else:
+
+    print("Could not connect")
